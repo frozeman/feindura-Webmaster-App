fed6d3b6921bf939bec0f7f0f64406c37e2cfcfd
Fabian Vogelsteller <fabian@frozeman.de>
2011-09-13 18:05:17 -0400

    add new snycFeinduraAccounts file step1

---
+++ feindura Webmaster Tool/SyncsFeinduraAccounts.h
@@ -0,0 +1,53 @@
+//
+//  SyncFeinduras.h
+//  feindura Webmaster Tool
+//
+//  Created by Fabian Vogelsteller on 24.08.11.
+//  Copyright 2011 [frozeman.de]. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ASIFormDataRequest.h"
+#import "Reachability.h"
+
+@class RootViewController;
+
+@interface SyncFeinduraAccounts : NSObject {
+    NSString *settingsFilePath;
+    NSMutableDictionary *dataBase;
+    ASIFormDataRequest *httpRequest;
+    Reachability *internetReachable;
+    Reachability *hostReachable;
+    BOOL internetActive;
+    
+    RootViewController *delegate;
+}
+
+#pragma mark Properties
+
+@property(nonatomic,retain) NSString *settingsFilePath;
+@property(nonatomic,retain) NSMutableDictionary *dataBase;
+@property(nonatomic,retain) ASIFormDataRequest *httpRequest;
+@property(nonatomic,retain) Reachability *internetReachable;
+@property(nonatomic,retain) Reachability *hostReachable;
+@property(nonatomic,assign) BOOL internetActive;
+@property(nonatomic,retain) RootViewController *delegate;
+
+#pragma mark Methods
+
+-(SyncFeinduraAccounts *)initWithoutInternet;
+-(BOOL)setSettingsPath;
+
+-(void)loadAccounts;
+-(void)saveAccounts;
+-(BOOL)updateAccounts;
+
+#pragma mark Delegates
+
+- (void)requestStarted:(ASIHTTPRequest *)request;
+- (void)request:(ASIHTTPRequest *)request didReceiveResponseHeaders:(NSDictionary *)responseHeaders;
+- (void)requestFinished:(ASIHTTPRequest *)request;
+- (void)requestFailed:(ASIHTTPRequest *)request;
+
+
+@end
